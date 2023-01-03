50af74a834ab3ec311b33789c0fe0f43a32dba0e
Vivek Kumar <91373023+Vivek-04022001@users.noreply.github.com>
2023-01-03 15:42:11 +0530

    overlay created

--- building space X website/Project/app.js
+++ building space X website/Project/app.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('menu-btn');
+const overlay = document.querySelector('.overlay');
 btn.addEventListener('click', navToggle);
 function navToggle(){
-    btn.classList.toggle('open')
+    btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show');
 }
