48ad22af1ea415e9bcfbf0cbea6802e4ea5f3e35
Vivek Kumar <91373023+Vivek-04022001@users.noreply.github.com>
2023-01-03 16:03:26 +0530

    slide menu

--- building space X website/Project/app.js
+++ building space X website/Project/app.js
@@ -1,8 +1,12 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.querySelector('.overlay');
+const mobileMenu = document.querySelector('.mobile-main-menu');
 btn.addEventListener('click', navToggle);
+
 function navToggle(){
     btn.classList.toggle('open');
     overlay.classList.toggle('overlay-show');
-    document.body.classList.toggle('stop-scrolling')
+    document.body.classList.toggle('stop-scrolling');
+    mobileMenu.classList.toggle('show-menu');
+
 }
